43ebdba38ea879e3052e384d60e251eb68f3ba15
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-07-07 09:57:26 +0600

    Why choose and statistics section designed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,19 @@
-import Image from "next/image";
+import Hero from "@/components/home/Hero";
+import Statistics from "@/components/home/Statistics";
+import WhyChoose from "@/components/home/WhyChoose";
 
 export default function Home() {
+  const stats = {
+    totalEvents: 30,
+    totalAttendees: 4000,
+    totalOrgs: 10,
+  };
   return (
     <div>
       <main>
-        <p>Home page</p>
+        <Hero />
+        <WhyChoose />
+        <Statistics stats={stats} />
       </main>
     </div>
   );
