2c7baf50a424cb154710710248f3e742b7237e0a
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-07-11 14:22:49 +0600

    Event delete and update api added

---
+++ src/app/(dashboardLayout)/dashboard/organizer/manage-events/page.jsx
@@ -0,0 +1,25 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { myEvents } from "@/lib/api/events/data";
+import { Suspense } from "react";
+import { Spinner } from "@heroui/react";
+import DashboardHeading from "@/components/dashboard/DashboardHeading";
+import ManageEventClient from "@/components/dashboard/ManageEventClient";
+
+const ManageEvent = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  const events = await myEvents(session?.user?.email);
+
+  return (
+    <div>
+      <DashboardHeading title="Manage Event" description="Manage event" />
+      <Suspense fallback={<Spinner />}>
+        <ManageEventClient events={events} />
+      </Suspense>
+    </div>
+  );
+};
+
+export default ManageEvent;
